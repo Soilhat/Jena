6524bf9bf4a3c0a5d5e4822407bc3d271689ad63
Soilhat <soilhatmohamed@gmail.com>
2019-03-14 20:25:34 +0100

    Jena1 and 2 creation

--- src/main/java/Jena1.java
+++ src/main/java/Jena1.java
@@ -1,30 +1,19 @@
 import org.apache.jena.rdf.model.*;
-import org.apache.jena.util.FileManager;
-
-import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Jena1 {
 
-    Model model;
+    private List<Person> tab;
 
-    public Jena1(){
-        // create an empty model
-        Model model = ModelFactory.createDefaultModel();
-        String inputFileName = "data/Movie.rdf";
-        // use the FileManager to find the input file
-        InputStream in = FileManager.get().open( inputFileName );
-        if (in == null) {
-            throw new IllegalArgumentException(
-                    "File: " + inputFileName + " not found");
-        }
-
-        // read the RDF/XML file
-        model.read(in, null);
+    public Jena1(Model model){
 
         // write it to standard out
         //model.write(System.out, "N-TRIPLES");
 
         StmtIterator iter = model.listStatements();
+        tab = new ArrayList<Person>();
+        List<Movie> movies = new ArrayList<Movie>();
 
         // print out the predicate, subject and object of each statement
         while (iter.hasNext()) {
@@ -33,18 +22,38 @@ public class Jena1 {
             Property  predicate = stmt.getPredicate();   // get the predicate
             RDFNode   object    = stmt.getObject();      // get the object
 
-            // System.out.print(subject.toString());
-            //System.out.print(" " + predicate.toString() + " ");
+            if(predicate.toString().contains("nationality")) tab.add(new Person((subject.toString())));
+            for (Person b : tab ) {
+                if(b.getId().equals(subject.toString())){
+                    if(predicate.toString().contains("name")) b.setName(object.toString());
+                    if(predicate.toString().contains("age")) b.setAge(object.toString().substring(0,2));
+                    if(predicate.toString().contains("nationality")) b.setNationality(object.toString());
+                    if(predicate.toString().contains("gender")) b.setGender(object.toString());
+                }
+            }
 
-            if(predicate.toString().contains("#name")){
-            if (object instanceof Resource) {
-                System.out.print(object.toString());
-            } else {
-                // object is a literal
-                System.out.print(" \"" + object.toString() + "\"");
-            }}
-
-            //System.out.println(" .");
+            if(predicate.toString().contains("language"))  movies.add(new Movie(subject.toString()));
+            for (Movie b : movies ) {
+                if(b.getId().equals(subject.toString())){
+                    if(predicate.toString().contains("year")) b.setYear(object.toString().substring(0,4));
+                    else if(predicate.toString().contains("title")) b.setTitle(object.toString());
+                    else if(predicate.toString().contains("country")) b.setCountry(object.toString());
+                    else if(predicate.toString().contains("language")) b.setLanguage(object.toString());
+                    else if(predicate.toString().contains("type")&& !object.toString().contains("NamedIndividuals")) b.setType(object.toString().split("#")[1]);
+                }
+            }
         }
+
+        /*for (Movie b: movies) {
+            System.out.println(b);
+        }
+
+        for (Person b: tab) {
+            System.out.println(b);
+        }*/
+    }
+
+    public List<Person> getPersons() {
+        return tab;
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,28 @@
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.util.FileManager;
+
+import java.io.InputStream;
+import java.util.List;
+
 public class main {
     public static void main(String[] args) {
-        Jena1 jena1 = new Jena1();
+        Model model = ModelFactory.createDefaultModel();
+        String inputFileName = "data/Movie.rdf";
+        // use the FileManager to find the input file
+        InputStream in = FileManager.get().open( inputFileName );
+        if (in == null) {
+            throw new IllegalArgumentException(
+                    "File: " + inputFileName + " not found");
+        }
+
+        // read the RDF/XML file
+        model.read(in, null);
+
+        Jena1 jena1 = new Jena1(model);
+        List<Person> personList = jena1.getPersons();
+        for(Person p : personList) System.out.println(p);
+        //Jena2 jena2 = new Jena2(model);
+        //Jena3 jena3 = new Jena3(model);
     }
 }
